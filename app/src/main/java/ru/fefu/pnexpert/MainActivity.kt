da6b6c956e3d627c06f31e52253664401d3cf91d
Rome4 <r.bortsov@yandex.ru>
2023-07-20 19:17:29 +0300

    add app icon and splash screen

--- app/src/main/java/ru/fefu/pnexpert/MainActivity.kt
+++ app/src/main/java/ru/fefu/pnexpert/MainActivity.kt
@@ -8,13 +8,37 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import ru.fefu.pnexpert.ui.theme.PNExpertTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        var isUiReady: Boolean by mutableStateOf(false)
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                delay(2000)
+                isUiReady = true
+            }
+        }
+
+        splashScreen.setKeepOnScreenCondition {
+            !isUiReady
+        }
+
         setContent {
             PNExpertTheme {
                 // A surface container using the 'background' color from the theme
