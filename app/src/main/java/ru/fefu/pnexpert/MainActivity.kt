4b7b52606e61aeca1a5270bda39bb4f18db81515
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-20 18:07:03 +1000

    add sf pro display fonts

--- app/src/main/java/ru/fefu/pnexpert/MainActivity.kt
+++ app/src/main/java/ru/fefu/pnexpert/MainActivity.kt
@@ -3,17 +3,12 @@ package ru.fefu.pnexpert
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.TextStyle
 import ru.fefu.pnexpert.ui.theme.PnExpertTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,22 +18,12 @@ class MainActivity : ComponentActivity() {
             PnExpertTheme() {
                 // A surface container using the 'background' color from the theme
 
-
-                Box(
-                    modifier = Modifier.fillMaxSize().background(
-                        PnExpertTheme.colors.gradients.GradientBlue
-                    )
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize(),
                 ) {
-                    Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        shape = CircleShape,
-                        color = Color.Transparent,
-                        contentColor = Color.White,
-                    ) {
-                        // Ваше содержимое
-                    }
-                }
 
+                }
             }
         }
     }
--- app/src/main/java/ru/fefu/pnexpert/ui/theme/PnExpertTypography.kt
+++ app/src/main/java/ru/fefu/pnexpert/ui/theme/PnExpertTypography.kt
@@ -1,12 +1,41 @@
 package ru.fefu.pnexpert.ui.theme
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.sp
+import ru.fefu.pnexpert.R
+
+private val SfProDisplay = FontFamily(
+    Font(R.font.sf_pro_display_semibold, FontWeight.W600),
+    Font(R.font.sf_pro_display_medium, FontWeight.W500),
+    Font(R.font.sf_pro_display_regular, FontWeight.W400),
+)
 
 val typographyPalette = PnExpertTypography().apply {
-//    this.title =
-//        PnExpertTypography.Title(
-//            bold_32 = TextStyle(
-//
-//            )
-//        )
+
+    this.title =
+        PnExpertTypography.Title(
+            bold_32 = TextStyle(
+                fontSize = 32.sp,
+
+                fontWeight = FontWeight(600),
+            ),
+            medium_32 = TextStyle(
+                fontSize = 32.sp,
+                lineHeight = 43.2.sp,
+
+                fontWeight = FontWeight(500),
+                color = Color(0xFF393939),
+            )
+        )
+
+
+
 }
