973a95df52297f77091001797ecf44cea7fe98c4
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-20 18:28:16 +1000

    add typography in PnExpertTheme

--- app/src/main/java/ru/fefu/pnexpert/MainActivity.kt
+++ app/src/main/java/ru/fefu/pnexpert/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import ru.fefu.pnexpert.ui.theme.PnExpertTheme
@@ -21,8 +23,18 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier
                         .fillMaxSize(),
+                    color = PnExpertTheme.colors.mainAppColors.AppBlueColor
                 ) {
-
+                    Column() {
+                        Text(
+                            text = "Дарова",
+                            style = PnExpertTheme.typography.title.medium_32
+                        )
+                        Text(
+                            text = "Дарова",
+                            style = PnExpertTheme.typography.title.bold_32
+                        )
+                    }
                 }
             }
         }
--- app/src/main/java/ru/fefu/pnexpert/ui/theme/PnExpertTypography.kt
+++ app/src/main/java/ru/fefu/pnexpert/ui/theme/PnExpertTypography.kt
@@ -1,14 +1,9 @@
 package ru.fefu.pnexpert.ui.theme
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.sp
 import ru.fefu.pnexpert.R
 
