2b4fd2fa90544afc590a17f478fdbce4771c466c
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-20 12:57:30 +1000

    create PnExpertColors tokens

--- app/src/main/java/ru/fefu/pnexpert/ui/theme/Theme.kt
+++ app/src/main/java/ru/fefu/pnexpert/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -37,6 +38,47 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+//PN_Expert tokens
+data class PnExpertColors(
+    //MainAppColors
+    val AppDarkColor: Color = Color(0xFF393939),
+    val AppBlueColor: Color = Color(0xFF5668E8),
+    val AppPinkLightColors: Color = Color(0xFFF9C9DC),
+    val AppGreyLightColor: Color = Color(0xFFF4F4F4),
+    val AppGreyDarkColor: Color = Color(0xFF8B8B8B),
+    val AppBlueLightColor:Color = Color(0xFF6AEBEB),
+    val AppWhiteColor:Color = Color(0xFFFFFFFF),
+    val AppPinkDarkColor:Color = Color(0xFFF25F8E),
+
+    //ButtonsColors
+    val ButtonNormalBlueColor:Color = Color(0xFF5668E8),
+    val ButtonNormalRedColor:Color = Color(0xFFF25F8E),
+
+    val ButtonHoverBlueColor:Color = Color(0xFF3E50CE),
+    val ButtonHoverRedColor:Color = Color(0xFFEB4E80),
+    val ButtonHoverLightColor:Color = Color(0xFFE5E9FF),
+
+    val ButtonPressedBlueColor:Color = Color(0xFF2C3CAC),
+    val ButtonPressedRedColor:Color = Color(0xFFD6376A),
+    val ButtonPressedLightColor: Color = Color(0xFFC7CEFF),
+
+    val ButtonInactiveColor:Color = Color(0xFF929FFF),
+
+    //FontColors
+    val FontDarkColor:Color = Color(0xFF393939),
+    val FontBlueColor: Color = Color(0xFF5668E8),
+    val FontGreyColor: Color = Color(0xFF8B8B8B),
+    val FontWhiteColor:Color = Color(0xFFFFFFFF),
+
+    //IconColors
+    val IconBlueColor: Color = Color(0xFF5668E8),
+    val IconBlueLightColor: Color = Color(0xFF5FABF2),
+    val IconRedColor:Color = Color(0xFFF24726),
+    val IconPinkColor:Color = Color(0xFFF25F8E),
+    val IconGreenColor:Color = Color(0xFF149D17),
+    val IconOrangeColor:Color = Color(0xFFFA8E10),
+)
+
 @Composable
 fun PNExpertTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -62,9 +104,11 @@ fun PNExpertTheme(
         }
     }
 
+
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
+
 }
