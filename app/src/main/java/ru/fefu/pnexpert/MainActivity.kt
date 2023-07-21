6c2910b46bcd443d2f3fd5b4e692bc2723334019
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-21 12:29:58 +1000

    add splash screen librarie

--- app/src/main/java/ru/fefu/pnexpert/MainActivity.kt
+++ app/src/main/java/ru/fefu/pnexpert/MainActivity.kt
@@ -3,26 +3,20 @@ package ru.fefu.pnexpert
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import ru.fefu.pnexpert.ui.theme.PnExpertTheme
 
 
