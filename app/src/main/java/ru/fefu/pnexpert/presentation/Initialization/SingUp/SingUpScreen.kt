be56fa9752b9d83454619c0396e54ac593cdb4b6
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-22 17:23:45 +1000

    add alternative sing up buttons in SingUpScreen

--- app/src/main/java/ru/fefu/pnexpert/presentation/Initialization/SingUp/SingUpScreen.kt
+++ app/src/main/java/ru/fefu/pnexpert/presentation/Initialization/SingUp/SingUpScreen.kt
@@ -1,6 +1,8 @@
 package ru.fefu.pnexpert.presentation.Initialization.SingUp
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -29,6 +32,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
@@ -59,8 +63,69 @@ fun SingUpScreen() {
         ) {
             Spacer(modifier = Modifier.height(80.dp))
             SingUpInputFields()
+            Spacer(modifier = Modifier.height(120.dp))
+            AlternativeSingUp()
         }
     }
+}
+
+@Composable
+fun AlternativeSingUp(){
+
+    val buttonShadow = 4.dp
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Войти через социальные сети",
+            style = PnExpertTheme.typography.text.regular_16,
+            color = PnExpertTheme.colors.textColors.FontGreyColor,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            IconButton(
+                onClick = {}
+            ) {
+                Image(
+                    modifier = Modifier.shadow(buttonShadow, CircleShape),
+                    painter = painterResource(id = R.drawable.ok_icon),
+                    contentDescription = "okSingUpIcon"
+                )
+            }
+            IconButton(
+                onClick = {}
+            ) {
+                Image(
+                    modifier = Modifier.shadow(buttonShadow, CircleShape),
+                    painter = painterResource(id = R.drawable.google_icon),
+                    contentDescription = "okSingUpIcon"
+                )
+            }
+            IconButton(
+                onClick = {}
+            ) {
+                Image(
+                    modifier = Modifier.shadow(buttonShadow, CircleShape),
+                    painter = painterResource(id = R.drawable.whatsapp_icon),
+                    contentDescription = "okSingUpIcon"
+                )
+            }
+            IconButton(
+                onClick = {}
+            ) {
+                Image(
+                    modifier = Modifier.shadow(buttonShadow, CircleShape),
+                    painter = painterResource(id = R.drawable.telegram_icon),
+                    contentDescription = "okSingUpIcon"
+                )
+            }
+        }
+    }
+    
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
