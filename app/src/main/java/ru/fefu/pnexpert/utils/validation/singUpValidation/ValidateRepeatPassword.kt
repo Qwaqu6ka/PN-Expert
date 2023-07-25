2dfaac269e2a976d3086c3dda2b717483992e164
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-25 12:00:56 +1000

    create ValidateRepeatPassword class

---
+++ app/src/main/java/ru/fefu/pnexpert/utils/validation/singUpValidation/ValidateRepeatPassword.kt
@@ -0,0 +1,23 @@
+package ru.fefu.pnexpert.utils.validation.singUpValidation
+
+import ru.fefu.pnexpert.utils.validation.models.ValidationResult
+
+class ValidateRepeatPassword {
+    operator fun invoke(password: String, repeatPassword: String): gValidationResult {
+        if (repeatPassword.isBlank()) {
+            return ValidationResult(
+                success = false,
+                errorMessage = "Поле не должно быть пустым"
+            )
+        }
+        if (password != repeatPassword){
+            return ValidationResult(
+                success = false,
+                errorMessage = "Пароли не совпадают"
+            )
+        }
+        return ValidationResult(
+            success = true
+        )
+    }
+}
