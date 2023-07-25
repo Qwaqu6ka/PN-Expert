b31aedc79604842f4d3162a5e0fd46adee8cdd27
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-25 11:58:44 +1000

    create ValidatePhoneNumber class

---
+++ app/src/main/java/ru/fefu/pnexpert/utils/validation/singUpValidation/ValidatePhoneNumber.kt
@@ -0,0 +1,24 @@
+package ru.fefu.pnexpert.utils.validation.singUpValidation
+
+import ru.fefu.pnexpert.utils.validation.models.ValidationResult
+import javax.inject.Inject
+
+class ValidatePhoneNumberUseCase {
+    operator fun invoke(phoneNumber: String): ValidationResult {
+        if (phoneNumber.isBlank()) {
+            return ValidationResult(
+                success = false,
+                errorMessage = "Поле не должно быть пустым"
+            )
+        }
+        if(!(phoneNumber.length == 12 && phoneNumber[0] == '+')&&(phoneNumber.length != 11)){
+            return ValidationResult(
+                success = false,
+                errorMessage = "Номер телефона должен состоять из 11 символов, не учитывая символ '+'"
+            )
+        }
+        return ValidationResult(
+            success = true
+        )
+    }
+}
