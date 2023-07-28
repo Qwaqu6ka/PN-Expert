b19aa56357f4d03abeb2e78e65a1a64ffe685fa7
Farma20 <kustov.ius@students.dvfu.ru>
2023-07-28 14:56:55 +1000

    create timer in ConformNumberPage

--- app/src/main/java/ru/fefu/pnexpert/presentation/initialization/registration/conform_phone/ConformNumberPage.kt
+++ app/src/main/java/ru/fefu/pnexpert/presentation/initialization/registration/conform_phone/ConformNumberPage.kt
@@ -1,7 +1,10 @@
 package ru.fefu.pnexpert.presentation.initialization.registration.conform_phone
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
@@ -17,17 +21,38 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+import androidx.compose.ui.graphics.PointMode
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 import ru.fefu.pnexpert.presentation.initialization.registration.RegistrationViewModel
 import ru.fefu.pnexpert.presentation.initialization.registration.navigation.RegistrationNavigationRoute
 import ru.fefu.pnexpert.presentation.theme.PnExpertTheme
+import java.lang.Math.PI
+import java.lang.Math.cos
+import java.lang.Math.sin
+import kotlin.math.cos
+import kotlin.math.sin
 
 private val CURRENT_PAGE = RegistrationNavigationRoute.ConformPhoneScreen
 
@@ -39,11 +64,12 @@ fun ConformNumberPage(viewModel: RegistrationViewModel) {
     Column(
         modifier = Modifier
         .padding(horizontal = 16.dp),
+
     ) {
         Spacer(modifier = Modifier.height(30.dp))
         InputCodeFields()
         Spacer(modifier = Modifier.height(80.dp))
-        MessageTimer()
+        MessageTimer(totalTime = 100L * 310L,)
         Spacer(modifier = Modifier.height(80.dp))
         TextRepeatCode()
         Spacer(modifier = Modifier.height(50.dp))
@@ -95,10 +121,35 @@ fun TextRepeatCode() {
 }
 
 @Composable
-private fun MessageTimer() {
+private fun MessageTimer(
+    totalTime: Long,
+    initialValue: Float = 1f,
+) {
+    var size by remember {
+        mutableStateOf(IntSize.Zero)
+    }
+    var value by remember {
+        mutableStateOf(initialValue)
+    }
+    var currentTime by remember {
+        mutableStateOf(totalTime)
+    }
+    var isTimerRunning by remember {
+        mutableStateOf(true)
+    }
+    LaunchedEffect(key1 = currentTime, key2 = isTimerRunning) {
+        if(currentTime > 0 && isTimerRunning) {
+            delay(100L)
+            currentTime -= 100L
+            value = currentTime / totalTime.toFloat()
+        }
+    }
     Text(
         modifier = Modifier.fillMaxWidth(),
-        text = "00:30",
+        text = if ((currentTime / 1000L) >= 10)
+                    "00:${currentTime / 1000L}"
+               else
+                    "00:0${currentTime / 1000L}",
         textAlign = TextAlign.Center,
         style = PnExpertTheme.typography.title.medium_32,
         color = PnExpertTheme.colors.mainAppColors.AppPinkDarkColor
