7cacfa7c2096792834fed70e3e647c921c0441b3
Farma20 <kustov.ius@students.dvfu.ru>
2023-09-08 14:07:06 +1000

    Create CustomCameraRepository functions

--- features/photo-tests-impl/src/main/java/ru/fefu/photo_tests_impl/data/CustomCameraRepository.kt
+++ features/photo-tests-impl/src/main/java/ru/fefu/photo_tests_impl/data/CustomCameraRepository.kt
@@ -1,19 +1,93 @@
 package ru.fefu.photo_tests_impl.data
 
+import android.content.ContentValues
 import android.content.Context
+import android.os.Build
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import ru.fefu.photo_tests_impl.domain.repositories.PhotoTestsCameraRepository
+import java.text.SimpleDateFormat
+import java.util.Locale
+import javax.inject.Inject
 
-class CustomCameraRepository:PhotoTestsCameraRepository {
+class CustomCameraRepository @Inject constructor(
+    private val cameraProvider: ProcessCameraProvider,
+    private val selector:CameraSelector,
+    private val preview: Preview,
+    private val imageAnalysis: ImageAnalysis,
+    private val imageCapture: ImageCapture,
+) :PhotoTestsCameraRepository {
     override suspend fun captureAndSaveImage(context: Context) {
-        TODO("Not yet implemented")
+        val name = SimpleDateFormat(
+            "yyyy-MM-dd-HH-mm-ss-SSS",
+            Locale.ENGLISH
+        ).format(System.currentTimeMillis())
+
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT > 28){
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/Photo-Tests-Pictures")
+            }
+        }
+
+        //for capture output
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(
+                context.contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+            .build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(context),
+            object : ImageCapture.OnImageSavedCallback{
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    Toast.makeText(
+                        context,
+                        "Saved image ${outputFileResults.savedUri!!}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Toast.makeText(
+                        context,
+                        "Some error occurred ${exception.message}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        )
     }
 
     override suspend fun showCameraPreview(
         previewView: PreviewView,
         lifecycleOwner: LifecycleOwner
     ) {
-        TODO("Not yet implemented")
+        preview.setSurfaceProvider(previewView.surfaceProvider)
+        try {
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(
+                lifecycleOwner,
+                selector,
+                preview,
+                imageAnalysis,
+                imageCapture
+            )
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
     }
 }
