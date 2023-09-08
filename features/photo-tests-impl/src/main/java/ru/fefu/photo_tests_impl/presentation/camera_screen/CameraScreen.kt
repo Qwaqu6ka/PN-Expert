8952c96819caf88741dc51daf0226b0ef33f30e7
Farma20 <kustov.ius@students.dvfu.ru>
2023-09-08 14:46:10 +1000

    create Camera screen in presentation layer in photo-tests-impl

--- features/photo-tests-impl/src/main/java/ru/fefu/photo_tests_impl/presentation/camera_screen/CameraScreen.kt
+++ features/photo-tests-impl/src/main/java/ru/fefu/photo_tests_impl/presentation/camera_screen/CameraScreen.kt
@@ -2,9 +2,32 @@ package ru.fefu.photo_tests_impl.presentation.camera_screen
 
 import android.Manifest
 import android.os.Build
+import android.widget.Toast
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import ru.fefu.photo_test_impl.R
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun CameraScreen(
     viewModel: CameraScreenViewModel = hiltViewModel()
@@ -15,4 +38,66 @@ fun CameraScreen(
             Manifest.permission.WRITE_EXTERNAL_STORAGE
         )
     }else listOf(Manifest.permission.CAMERA)
+
+    val permissionState = rememberMultiplePermissionsState(permissions = permission)
+
+    if (!permissionState.allPermissionsGranted){
+        SideEffect {
+            permissionState.launchMultiplePermissionRequest()
+        }
+    }
+
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
+    val screenWith = configuration.screenWidthDp.dp
+    var previewView:PreviewView
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        if (permissionState.allPermissionsGranted){
+            Box(
+                modifier = Modifier
+                    .height(screenHeight * 0.85f)
+                    .width(screenWith)
+            ){
+                AndroidView(
+                    factory = {
+                        previewView = PreviewView(it)
+                        viewModel.showCameraPreview(previewView, lifecycleOwner)
+                        previewView
+                    },
+                    modifier = Modifier
+                        .height(screenHeight * 0.85f)
+                        .width(screenWith)
+                )
+            }
+        }
+        Box(
+            modifier = Modifier
+                .height(screenHeight*0.15f),
+            contentAlignment = Alignment.Center
+        ){
+            IconButton(onClick = {
+                if (permissionState.allPermissionsGranted){
+                    viewModel.captureAndSave(context)
+                }else{
+                    Toast.makeText(
+                        context,
+                        "Please accept permission in app settings",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_camera_24),
+                    contentDescription = "cameraButton",
+                    tint = Color.Red
+                )
+            }
+        }
+    }
 }
