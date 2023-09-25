3b8602d35bd248be04a7533bdaf55b52208a9359
Farma20 <kustov.ius@students.dvfu.ru>
2023-09-25 19:34:00 +1000

    create PhotoTestAnswerToReading class

--- features/photo-tests-impl/src/main/java/ru/fefu/photo_tests_impl/presentation/photo_test_screen/PhotoTestScreenViewModel.kt
+++ features/photo-tests-impl/src/main/java/ru/fefu/photo_tests_impl/presentation/photo_test_screen/PhotoTestScreenViewModel.kt
@@ -1,19 +1,21 @@
 package ru.fefu.photo_tests_impl.presentation.photo_test_screen
 
 import android.net.Uri
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import ru.fefu.photo_tests_impl.presentation.models.PhotoTestAnswerEntities
 
 
 class PhotoTestScreenViewModel:ViewModel() {
     private val _photoPath = mutableStateOf(Uri.EMPTY)
     val photoPath: State<Uri> = _photoPath
 
-//    private val userAnswers: UserAnswer =
+    private val userAnswers: PhotoTestAnswerEntities = PhotoTestAnswerEntities()
 
     fun setPhotoPath(photoPath:Uri){
         _photoPath.value = photoPath
     }
+
+
 }
